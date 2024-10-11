cb2f476215aeabd6ae260b07ba5abcb7bcfdf2ae
Syam <syamnarendra85@gmail.com>
2024-10-11 15:58:37 +0530

    temp deleted

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func main() {
 	router := gin.Default()
 	router.GET("/", func(res *gin.Context) {
 		res.JSON(http.StatusOK, gin.H{
-			"message": "Hello Bugghhhhhhy !!",
+			"message": "Hello Buggdddddddhhhhhhy !!",
 		})
 	})
 	router.POST("/create-bug", func(ctx *gin.Context) {
